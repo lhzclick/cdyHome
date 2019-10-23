522aa4df4843b665ce477cb5ea3656e4cfb4d6da
hegang8 <790974067@qq.com>
2019-10-23 18:00:59 +0800

    官网首页

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,10 @@
 /* eslint-disable */
-import '../../common/css/com.less';
-import './index.less';
-import {throttle} from '../../tools/utils';
-import './test'
-
-console.log('首页的js运行了～～')
+//import 'http://hfdzhg.cn/cdyCommon/js/jquery-3.4.1.min.js'
+import './index.css'
+import '../../common/js/config'
+var data = {
+    title:"<img style='margin-top: 7px' src='" + publicSrc + "/img/cdyHome/index1/logo.png' alt=''>"
+   };
+   var html = template("imgLogo",data);
+   console.log(html)
+   $('#imgLogo').html(html)
