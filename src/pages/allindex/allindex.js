7f0e9e359561c54b1250dbffa45f45a2f9eae44e
hegang8 <790974067@qq.com>
2019-10-29 10:40:28 +0800

    官网首页

--- src/pages/allindex/allindex.js
+++ src/pages/allindex/allindex.js
@@ -9,10 +9,15 @@ var img_src2 = 'http://hfdzhg.cn/img/cdyHome/index2/zhinengjianzu.png'
 window.next = function () {
     $('.img_left').attr('src', img_src2);
     $('.img_right').attr('src', img_src1);
+    $('.data_box').hide();
+    $('.data_box').eq(1).show();
 }
 window.prev = function () {
     $('.img_left').attr('src', img_src1);
     $('.img_right').attr('src', img_src2);
+    $('.data_box').hide();
+    $('.data_box').eq(0).show();
+
 }
 //鼠标移入大数据板块
 $('.choise_box').on({
@@ -25,3 +30,11 @@ $('.choise_box').on({
         $('.choise_tab').eq(0).show();
     }
 });
+//点击app下载
+$('.service-app-icon').on('click',function(){
+    $('.scanCode').slideDown();
+})
+$('.codeColse').on('click',function(event){
+    $('.scanCode').slideUp();
+    event.stopPropagation(); 
+})
